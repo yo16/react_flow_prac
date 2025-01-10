641f92afc65f0d5398173f5703b8b786bb616a0d
yo16 <xxyo1xx@gmail.com>
2025-01-10 17:42:38 +0900

    add Controls, Minimap, Background

--- app-react-flow-vite/src/App.tsx
+++ app-react-flow-vite/src/App.tsx
@@ -1,9 +1,13 @@
 import { useCallback } from 'react';
 import {
   ReactFlow,
+  MiniMap,
+  Controls,
+  Background,
   useNodesState,
   useEdgesState,
   addEdge,
+  BackgroundVariant,
 } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
  
@@ -25,14 +29,18 @@ export default function App() {
 
 
   return (
-    <div style={{ width: '95vw', height: '95vh', backgroundColor: "#eee" }}>
+    <div style={{ width: '95vw', height: '95vh', backgroundColor: "#fff" }}>
       <ReactFlow
         nodes={nodes}
         edges={edges}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
-      />
+      >
+        <Controls />
+        <MiniMap />
+        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+      </ReactFlow>
     </div>
   );
 }
